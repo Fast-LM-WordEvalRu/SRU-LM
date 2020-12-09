d8b274aa5be59734f6fac671eb0930cf23319e0d
Artem Skiba <a.skiba@dsys.ai>
2020-12-10 01:45:44 +0700

    add requirements parsing into setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from setuptools import setup, find_packages
 
+
+def load_requirements(path):
+    with open(path) as fin:
+        return [
+            line
+            for line in map(lambda l: l.strip(), fin.readlines())
+            if line and not line.startswith('#')
+        ]
+
+
+requirements = load_requirements('requirements.txt')
+
 setup(
     name='sru_lm',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
+    install_requires=requirements,
 )
